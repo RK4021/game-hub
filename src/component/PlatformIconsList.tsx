1b4b5efb7d7afabb3057a5098b0bb1100fa42687
Rahul Kumar <rahulkumar17082000@gmail.com>
2023-04-23 00:34:22 +0530

    displaying critic scpre

---
+++ src/component/PlatformIconsList.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
+import { color } from "framer-motion";
+
+const iconMap: { [key: string]: IconType } = {
+  pc: FaWindows,
+  FaPlaystation: FaPlaystation,
+  xbox: FaXbox,
+  nintendo: SiNintendo,
+  mac: FaApple,
+  linux: FaLinux,
+  ios: MdPhoneIphone,
+  android: FaAndroid,
+  web: BsGlobe,
+};
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconsList = ({ platforms }: Props) => {
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconsList;
